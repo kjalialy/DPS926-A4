abdca8a16ffbd2f4f58762b60ed63e0052ccd21d
Kyle Alialy <kylealialy09@gmail.com>
2021-12-08 22:25:55 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DPSA4-MusicApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
